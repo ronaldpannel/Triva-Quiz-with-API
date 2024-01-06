30b1f5c4b842944b35b225c619b665aaa9803b9f
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-01-06 15:22:21 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,126 @@
+const difficultyEl = document.getElementById("difficulty");
+const genreEl = document.getElementById("genre");
+const questionEl = document.getElementById("question");
+const scoreEl = document.getElementById("score");
+const correctAnswerEl = document.getElementById("correctAnswer");
+
+const nextQuestionBtnEl = document.getElementById("nextQuestionBtn");
+const getQuestionSetEl = document.getElementById("getQuestionSet");
+
+const buttonEl1 = document.getElementById("btn1");
+const buttonEl2 = document.getElementById("btn2");
+const buttonEl3 = document.getElementById("btn3");
+const buttonEl4 = document.getElementById("btn4");
+
+const buttons = document.querySelectorAll(".answerDiv button");
+
+let questionSetArray = [];
+let answersArray = [];
+let correctAnswer;
+let index = 0;
+let score = 0
+
+const apiURL = `https://opentdb.com/api.php?amount=10&category=9&type=multiple`;
+
+async function getQuestion() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    for (let i = 0; i <= data.results.length; i++) {
+      questionSetArray.push(data.results);
+    }
+    // console.log(data.results);
+    // console.log(questionSetArray);
+  } catch (error) {
+    console.log("something failed please try again");
+  }
+}
+
+function getNewQuestion() {
+  correctAnswerEl.innerText = ''
+  index++;
+  difficultyEl.innerText = questionSetArray[0][index].difficulty;
+  genreEl.innerText = questionSetArray[0][index].category;
+  questionEl.innerText = questionSetArray[0][index].question;
+  correctAnswer = questionSetArray[0][index].correct_answer;
+  scoreEl.innerText = score
+  
+  
+
+  answersArray = questionSetArray[0][index].incorrect_answers;
+  answersArray.push(questionSetArray[0][index].correct_answer);
+  ///shuffle Array
+  answersArray.sort(() => Math.random() - 0.5);
+  // console.log(answersArray);
+  console.log(correctAnswer)
+
+  buttonEl1.innerText = answersArray[0];
+  buttonEl2.innerText = answersArray[1];
+  buttonEl3.innerText = answersArray[2];
+  buttonEl4.innerText = answersArray[3];
+
+}
+
+
+getQuestionSetEl.addEventListener("click", () => {
+  questionSetArray = [];
+   buttons.forEach((button) => {
+     button.classList.remove("correct");
+     button.classList.remove("wrong");
+   });
+  getQuestion();
+});
+
+nextQuestionBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  getNewQuestion();
+   buttons.forEach((button) => {
+     button.classList.remove("correct");
+     button.classList.remove("wrong");
+   });
+});
+
+buttonEl1.addEventListener('click', () => {
+  if(buttonEl1.innerText === correctAnswer){
+    buttonEl1.classList.add('correct')
+    score++
+  }else{
+    buttonEl1.classList.add("wrong");
+    score--
+  }  
+  correctAnswerEl.innerText = correctAnswer;
+})
+
+buttonEl2.addEventListener('click', () => {
+  if (buttonEl2.innerText === correctAnswer) {
+    buttonEl2.classList.add("correct");
+     score++;
+  } else {
+    buttonEl2.classList.add("wrong");
+     score--
+  }
+  correctAnswerEl.innerText = correctAnswer;
+})
+
+buttonEl3.addEventListener('click', () => {
+  if (buttonEl3.innerText === correctAnswer) {
+    buttonEl3.classList.add("correct");
+     score++;
+  } else {
+    buttonEl3.classList.add("wrong");
+     score--
+  }
+  correctAnswerEl.innerText = correctAnswer;
+})
+
+buttonEl4.addEventListener("click", () => {
+  if (buttonEl4.innerText === correctAnswer) {
+    buttonEl4.classList.add("correct");
+     score++;
+  } else {
+    buttonEl4.classList.add("wrong");
+    score--;
+  }
+  correctAnswerEl.innerText = correctAnswer;
+});
