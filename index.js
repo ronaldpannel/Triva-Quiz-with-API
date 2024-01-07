e1ffe8e3dd03fdbf3443f01459b997a9407f9ccd
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-01-07 15:14:49 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const genreEl = document.getElementById("genre");
 const questionEl = document.getElementById("question");
 const scoreEl = document.getElementById("score");
 const correctAnswerEl = document.getElementById("correctAnswer");
+const gameOverDisplayEl = document.getElementById("gameOverDisplay");
+const finalScoreEl = document.getElementById("finalScore");
+
 
 const nextQuestionBtnEl = document.getElementById("nextQuestionBtn");
 const getQuestionSetEl = document.getElementById("getQuestionSet");
+const startGameBtnEl = document.getElementById("startGameBtn");
 
 const buttonEl1 = document.getElementById("btn1");
 const buttonEl2 = document.getElementById("btn2");
@@ -18,7 +22,9 @@ let questionSetArray = [];
 let answersArray = [];
 let correctAnswer;
 let index = 0;
-let score = 0
+let score = 0;
+const maxQuestion = 3
+let questionCount = 0;
 
 const apiURL = `https://opentdb.com/api.php?amount=10&category=9&type=multiple`;
 
@@ -30,94 +36,98 @@ async function getQuestion() {
     for (let i = 0; i <= data.results.length; i++) {
       questionSetArray.push(data.results);
     }
-    // console.log(data.results);
-    // console.log(questionSetArray);
+    console.log(questionCount);
   } catch (error) {
     console.log("something failed please try again");
   }
 }
 
 function getNewQuestion() {
-  correctAnswerEl.innerText = ''
+  correctAnswerEl.innerText = "";
   index++;
   difficultyEl.innerText = questionSetArray[0][index].difficulty;
   genreEl.innerText = questionSetArray[0][index].category;
   questionEl.innerText = questionSetArray[0][index].question;
   correctAnswer = questionSetArray[0][index].correct_answer;
-  scoreEl.innerText = score
-  
-  
+  scoreEl.innerText = score;
 
   answersArray = questionSetArray[0][index].incorrect_answers;
   answersArray.push(questionSetArray[0][index].correct_answer);
   ///shuffle Array
   answersArray.sort(() => Math.random() - 0.5);
   // console.log(answersArray);
-  console.log(correctAnswer)
 
   buttonEl1.innerText = answersArray[0];
   buttonEl2.innerText = answersArray[1];
   buttonEl3.innerText = answersArray[2];
   buttonEl4.innerText = answersArray[3];
-
 }
 
+startGameBtnEl.addEventListener('click', () => {
+  location.reload()
+})
 
 getQuestionSetEl.addEventListener("click", () => {
   questionSetArray = [];
-   buttons.forEach((button) => {
-     button.classList.remove("correct");
-     button.classList.remove("wrong");
-   });
+  buttons.forEach((button) => {
+    button.classList.remove("correct");
+    button.classList.remove("wrong");
+  });
+
   getQuestion();
 });
 
 nextQuestionBtnEl.addEventListener("click", (e) => {
   e.preventDefault();
   getNewQuestion();
-   buttons.forEach((button) => {
-     button.classList.remove("correct");
-     button.classList.remove("wrong");
-   });
+  buttons.forEach((button) => {
+    button.classList.remove("correct");
+    button.classList.remove("wrong");
+  });
+  questionCount++;
+  if(questionCount == maxQuestion){
+    gameOverDisplayEl.classList.add("visible");
+    finalScoreEl.innerText = score
+  }
 });
 
-buttonEl1.addEventListener('click', () => {
-  if(buttonEl1.innerText === correctAnswer){
-    buttonEl1.classList.add('correct')
-    score++
-  }else{
+buttonEl1.addEventListener("click", () => {
+  if (buttonEl1.innerText === correctAnswer) {
+    buttonEl1.classList.add("correct");
+    score++;
+  } else {
     buttonEl1.classList.add("wrong");
-    score--
-  }  
+    score--;
+  }
   correctAnswerEl.innerText = correctAnswer;
-})
+});
 
-buttonEl2.addEventListener('click', () => {
+buttonEl2.addEventListener("click", () => {
   if (buttonEl2.innerText === correctAnswer) {
     buttonEl2.classList.add("correct");
-     score++;
+    score++;
   } else {
     buttonEl2.classList.add("wrong");
-     score--
+    score--;
   }
   correctAnswerEl.innerText = correctAnswer;
-})
+});
 
-buttonEl3.addEventListener('click', () => {
+buttonEl3.addEventListener("click", () => {
   if (buttonEl3.innerText === correctAnswer) {
     buttonEl3.classList.add("correct");
-     score++;
+    score++;
   } else {
     buttonEl3.classList.add("wrong");
-     score--
+    score--;
   }
   correctAnswerEl.innerText = correctAnswer;
-})
+});
 
 buttonEl4.addEventListener("click", () => {
   if (buttonEl4.innerText === correctAnswer) {
     buttonEl4.classList.add("correct");
-     score++;
+    score++;
   } else {
     buttonEl4.classList.add("wrong");
     score--;
